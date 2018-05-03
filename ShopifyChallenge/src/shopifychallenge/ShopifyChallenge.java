f336c18a75e25fcc5c841c1c47070516b4e6baca
unknown <tpham20908@gmail.com>
2018-05-03 13:35:38 -0400

    reset ShopifyChallenge

--- ShopifyChallenge/src/shopifychallenge/ShopifyChallenge.java
+++ ShopifyChallenge/src/shopifychallenge/ShopifyChallenge.java
@@ -1,40 +1,9 @@
 package shopifychallenge;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class ShopifyChallenge {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        try {
 
-            URL url = new URL("http://backend-challenge-fall-2018.herokuapp.com/carts.json?id=1&page=1");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-     
-            if (conn.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP error code : "
-                                    + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                    System.out.println(output);
-            }
-
-            conn.disconnect();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
