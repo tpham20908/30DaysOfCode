e87a831630652dc0e2392522b67ca2e513b0a03b
unknown <tpham20908@gmail.com>
2018-05-04 14:04:52 -0400

    Factorial using Recursive

---
+++ Factorial/src/factorial/Factorial.java
@@ -0,0 +1,21 @@
+package factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int factorial(int n) {
+        // Complete this function
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int result = factorial(n);
+    System.out.println(result);
+    }
+}
