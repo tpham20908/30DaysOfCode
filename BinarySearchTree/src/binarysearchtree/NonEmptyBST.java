ff7e6da3571e0582be51370871fa1effc7635b6f
unknown <tpham20908@gmail.com>
2018-05-07 16:14:19 -0400

    scaffold NonEmptyBST and EmptyBST implement Tree

--- BinarySearchTree/src/binarysearchtree/NonEmptyBST.java
+++ BinarySearchTree/src/binarysearchtree/NonEmptyBST.java
@@ -1,5 +1,27 @@
 package binarysearchtree;
 
-class NonEmptyBST<T> {
-    
+class NonEmptyBST<D extends Comparable> implements Tree<D> {
+
+    public NonEmptyBST() {
+    }
+
+    @Override
+    public boolean isEmpty() {
+        
+    }
+
+    @Override
+    public int cardinality() {
+        
+    }
+
+    @Override
+    public boolean member(D ele) {
+        
+    }
+
+    @Override
+    public NonEmptyBST<D> add(D ele) {
+        
+    }
 }
