55b0106e404639e258bdf0bf0193829f3a4af86d
unknown <tpham20908@gmail.com>
2018-05-07 21:43:28 -0400

    created BinarySearchTree

--- BinarySearchTree/src/binarysearchtree/EmptyBST.java
+++ BinarySearchTree/src/binarysearchtree/EmptyBST.java
@@ -7,22 +7,22 @@ public class EmptyBST<D extends Comparable> implements Tree<D> {
 
     @Override
     public boolean isEmpty() {
-        
+        return true;
     }
 
     @Override
     public int cardinality() {
-        
+        return 0;
     }
 
     @Override
     public boolean member(D ele) {
-        
+        return false;
     }
 
     @Override
     public NonEmptyBST<D> add(D ele) {
-        
+        return new NonEmptyBST<D>(ele);
     }
     
 }
--- BinarySearchTree/src/binarysearchtree/NonEmptyBST.java
+++ BinarySearchTree/src/binarysearchtree/NonEmptyBST.java
@@ -1,27 +1,56 @@
 package binarysearchtree;
 
 class NonEmptyBST<D extends Comparable> implements Tree<D> {
+    
+    D data;
+    Tree<D> left;
+    Tree<D> right;
 
-    public NonEmptyBST() {
+    public NonEmptyBST(D ele) {
+        data = ele;
+        left = new EmptyBST<D>();
+        right = new EmptyBST<D>();
+    }
+    
+    public NonEmptyBST(D ele, Tree<D> leftTree, Tree<D> rightTree) {
+        data = ele;
+        left = leftTree;
+        right = rightTree;
     }
 
     @Override
     public boolean isEmpty() {
-        
+        return false;
     }
 
     @Override
     public int cardinality() {
-        
+        return 1 + left.cardinality() + right.cardinality();
     }
 
     @Override
     public boolean member(D ele) {
-        
+        if (data == ele) {
+            return true;
+        } else {
+            if (ele.compareTo(data) < 0) {
+                return left.member(ele);
+            } else {
+                return right.member(ele);
+            }
+        }
     }
 
     @Override
     public NonEmptyBST<D> add(D ele) {
-        
+        if (data == ele) {
+            return this;
+        } else {
+            if (ele.compareTo(data) < 0) {
+                return new NonEmptyBST(data, left.add(ele), right);
+            } else {
+                return new NonEmptyBST(data, left, right.add(ele));
+            }
+        }
     }
 }
