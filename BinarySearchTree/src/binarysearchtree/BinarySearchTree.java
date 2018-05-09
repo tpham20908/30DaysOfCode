4165e47e4513e443f77fca8625626d6bef32cf1c
unknown <tpham20908@gmail.com>
2018-05-09 11:45:59 -0400

    made checkIsEmpty method throws Exception

--- BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
+++ BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -2,7 +2,7 @@ package binarysearchtree;
 
 public class BinarySearchTree {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         EmptyBST e = new EmptyBST();
         NonEmptyBST n = new NonEmptyBST(22);
--- BinarySearchTree/src/binarysearchtree/Testers.java
+++ BinarySearchTree/src/binarysearchtree/Testers.java
@@ -2,17 +2,19 @@ package binarysearchtree;
 
 public class Testers {
     
-    public static void checkIsEmpty(Tree t) {
+    public static void checkIsEmpty(Tree t) throws Exception {
         // if the tree t is an instance of EmptyBST --> t.isEmpty() -> true
         if (t instanceof EmptyBST) {
-            if (t.isEmpty()) {
-                System.out.println("All is good, the tree is an EmptyBST and it is empty");
+            if (!t.isEmpty()) {
+                throw new Exception("All is not good, the tree is an EmptyBST "
+                        + "and it is not empty");
             }
         }
         // if the tree t is an instance of NonEmptyBST --> t.isEmpty() -> false
         else if (t instanceof NonEmptyBST) {
-            if (!t.isEmpty()) {
-                System.out.println("All is good, the tree is an NonEmptyBST and it is non-empty");
+            if (t.isEmpty()) {
+                throw new Exception("All is not good, the tree is an NonEmptyBST "
+                        + "and it is not non-empty");
             }
         }
     }
